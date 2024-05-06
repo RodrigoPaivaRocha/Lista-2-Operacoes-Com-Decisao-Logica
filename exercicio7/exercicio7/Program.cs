be4ed05eb12c6e369985ad478a62580fac574897
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-05-06 20:43:33 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Digite o valor do lado 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do lado 2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do lado 3: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+                if (a == b && a == c)
+                    Console.WriteLine("Equilátero");
+                else
+                    if (a == b || a == c || b == c)
+                    Console.WriteLine("Isósceles");
+                else
+                    Console.WriteLine("Escaleno");
+            else
+                Console.WriteLine("Não forma triângulo");
+
+            Console.ReadKey();
+        }
+    }
+}
