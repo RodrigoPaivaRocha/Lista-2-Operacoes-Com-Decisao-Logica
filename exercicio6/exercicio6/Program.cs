ac22cab5fabc84160e9ffb481cbc17a889d38276
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-12 12:31:24 -0300

    Add files via upload

---
+++ exercicio6/exercicio6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, peso, relacao;
+
+            Console.WriteLine("Digite sua altura: ");
+            altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            relacao = peso / (altura * altura);
+
+            if (relacao < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (relacao >= 20 && relacao < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else
+            {
+                Console.WriteLine("Acima do peso");
+            }  
+            
+            Console.ReadKey();
+        }
+    }
+}
