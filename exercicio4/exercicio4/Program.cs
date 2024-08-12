0b24525abccfaefad8446a6d58a9b8a10ada0d95
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-12 12:29:36 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRet, alturaRet, areaRet;
+
+            Console.WriteLine("Digite a base do retângulo: ");
+            baseRet = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a base do retângulo: ");
+            alturaRet = double.Parse(Console.ReadLine());
+
+            areaRet = baseRet * alturaRet;
+
+            if (areaRet > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("O valor da área é: " + areaRet);
+            }
+            Console.ReadKey();
+        }
+    }
+}
