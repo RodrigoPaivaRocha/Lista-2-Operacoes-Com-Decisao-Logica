d06a0f98b2be0dce3a8227d67e8ccfd4de3ec2f6
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-12 12:28:08 -0300

    Add files via upload

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, maior = 0;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro número: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                maior = n1;
+            }
+            else
+            {
+                maior = n2;
+            }
+            if (n3 > maior)
+            {
+                maior = n3;
+            }
+            Console.WriteLine("O(s) maior(es) número(s) é(são): " + maior);
+
+            Console.ReadKey();
+        }
+    }
+}
