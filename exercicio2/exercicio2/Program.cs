57d6418b9b2830f6ca66b304ae8e0510d890d027
Rodrigo Paiva Rocha <166325000+RodrigoPaivaRocha@users.noreply.github.com>
2024-08-12 12:27:44 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+
+            Console.WriteLine("Digite um número: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            if (n1 == n2)
+            {
+                Console.WriteLine("Os números são idênticos");
+            }
+            else if (n1 > n2)
+            {
+                Console.WriteLine("O primeiro número é maior que o segundo");
+            }
+            else
+            {
+                Console.WriteLine("O segundo número é maior que o primeiro");
+            }
+            Console.ReadKey();
+        }
+    }
+}
